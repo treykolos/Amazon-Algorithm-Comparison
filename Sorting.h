2d321f34945ea6129004c0235c0b53734396359d
qwe <76127728+y123q@users.noreply.github.com>
2023-12-04 23:19:35 -0500

    Update Sorting.h

--- Sorting.h
+++ Sorting.h
@@ -10,139 +10,143 @@
 #include "Listing.h"
 #include "CSV.h"
 
-std::map<std::string, Listing> merge(std::string sortBy, std::map<std::string, Listing>& left, std::map<std::string, Listing>& right) {
-
-    std::map<std::string, Listing> result;
-    auto left_it = left.begin();
-    auto right_it = right.begin();
-
-    while (left_it != left.end() && right_it != right.end()) {
-        if (left_it->first < right_it->first) {
-            result[left_it->first] = left_it->second;
-            ++left_it;
-        }
-        else {
-            result[right_it->first] = right_it->second;
-            ++right_it;
-        }
-    }
-
-    // Copy the remaining elements from left and right maps
-    while (left_it != left.end()) {
-        result[left_it->first] = left_it->second;
-        ++left_it;
-    }
-
-    while (right_it != right.end()) {
-        result[right_it->first] = right_it->second;
-        ++right_it;
-    }
-
-    return result;
-
-    //int const subMapOne = mid - left + 1;
-    //int const subMapTwo = right - mid;
-
-    //std::map<std::string, Listing> leftMap, rightMap;
-
-    //auto it = listings.begin();
-    //std::advance(it, left);
-    //leftMap.insert(it, std::next(it, subMapOne));
-
-    //it = listings.begin();
-    //std::advance(it, mid + 1);
-    //rightMap.insert(it, std::next(it, subMapTwo));
-
-    //auto itLeft = leftMap.begin();
-    //auto itRight = rightMap.begin();
-    //auto itMerged = listings.begin();
-
-    //std::advance(itMerged, left);
-
-    //while (itLeft != leftMap.end() && itRight != rightMap.end()) {
-    //    //std::cout << "1" << std::endl;
-
-    //    bool compare;
 
 
-    //    if (sortBy == "price") {
-    //        compare = (*itLeft).second.getPrice() < (*itRight).second.getPrice();
-    //    }
-    //    else if (sortBy == "rating") {
-    //        compare = (*itLeft).second.getRating() < (*itRight).second.getRating();
-    //    }
-    //    else if (sortBy == "number_ratings") {
-    //        compare = (*itLeft).second.getNumRatings() < (*itRight).second.getNumRatings();
-    //    }
-    //    else if (sortBy == "name") {
-    //        compare = (*itLeft).second.getName() < (*itRight).second.getName();
-    //    }
-    //    else if (sortBy == "category") {
-    //        compare = (*itLeft).second.getCategory() < (*itRight).second.getCategory();
-    //    }
 
-    //    if (compare) {
-    //        itMerged->second = itLeft->second;
-    //        itLeft++;
+
+void merge(std::string sortBy, std::map<std::string, Listing>& listings, int const left, int const mid, int const right) {
+
+    //std::map<std::string, Listing> result;
+    //auto left_it = left.begin();
+    //auto right_it = right.begin();
+
+    //while (left_it != left.end() && right_it != right.end()) {
+    //    if (left_it->first < right_it->first) {
+    //        result[left_it->first] = left_it->second;
+    //        ++left_it;
     //    }
     //    else {
-    //        itMerged->second = itRight->second;
-    //        itRight++;
+    //        result[right_it->first] = right_it->second;
+    //        ++right_it;
     //    }
-    //    itMerged++;
     //}
 
-    //while (itLeft != leftMap.end()) {
-    //    //std::cout << "2" << std::endl;
-    //    itMerged->second = itLeft->second;
-    //    itLeft++;
-    //    itMerged++;
+    //// Copy the remaining elements from left and right maps
+    //while (left_it != left.end()) {
+    //    result[left_it->first] = left_it->second;
+    //    ++left_it;
     //}
 
-    //while (itRight != rightMap.end()) {
-    //    //std::cout << "3" << std::endl;
-
-    //    itMerged->second = itRight->second;
-    //    itRight++;
-    //    itMerged++;
+    //while (right_it != right.end()) {
+    //    result[right_it->first] = right_it->second;
+    //    ++right_it;
     //}
+
+    //return result;
+
+    int const subMapOne = mid - left + 1;
+    int const subMapTwo = right - mid;
+
+    std::map<std::string, Listing> leftMap, rightMap;
+
+    auto it = listings.begin();
+    std::advance(it, left);
+    leftMap.insert(it, std::next(it, subMapOne));
+
+    it = listings.begin();
+    std::advance(it, mid + 1);
+    rightMap.insert(it, std::next(it, subMapTwo));
+
+    auto itLeft = leftMap.begin();
+    auto itRight = rightMap.begin();
+    auto itMerged = listings.begin();
+
+    std::advance(itMerged, left);
+
+    while (itLeft != leftMap.end() && itRight != rightMap.end()) {
+        //std::cout << "1" << std::endl;
+
+        bool compare;
+
+
+        if (sortBy == "price") {
+            compare = (*itLeft).second.getPrice() < (*itRight).second.getPrice();
+        }
+        else if (sortBy == "rating") {
+            compare = (*itLeft).second.getRating() < (*itRight).second.getRating();
+        }
+        else if (sortBy == "number_ratings") {
+            compare = (*itLeft).second.getNumRatings() < (*itRight).second.getNumRatings();
+        }
+        else if (sortBy == "name") {
+            compare = (*itLeft).second.getName() < (*itRight).second.getName();
+        }
+        else if (sortBy == "category") {
+            compare = (*itLeft).second.getCategory() < (*itRight).second.getCategory();
+        }
+
+        if (compare) {
+            itMerged->second = itLeft->second;
+            itLeft++;
+        }
+        else {
+            itMerged->second = itRight->second;
+            itRight++;
+        }
+        itMerged++;
+    }
+
+    while (itLeft != leftMap.end()) {
+        //std::cout << "2" << std::endl;
+        itMerged->second = itLeft->second;
+        itLeft++;
+        itMerged++;
+    }
+
+    while (itRight != rightMap.end()) {
+        //std::cout << "3" << std::endl;
+
+        itMerged->second = itRight->second;
+        itRight++;
+        itMerged++;
+    }
 
 }
 
-//int q = 0;
+int q = 0;
 
-std::map<std::string, Listing> mergeSort(std::string sortBy, std::map<std::string, Listing>& listings) {
+void mergeSort(std::string sortBy, std::map<std::string, Listing>& listings, const int begin, const int end) {
 
-    if (listings.size() <= 1) {
-        return listings;
-    }
+    //if (listings.size() <= 1) {
+    //    return listings;
+    //}
 
     // Split the map into two halves
-    auto middle = std::next(listings.begin(), listings.size() / 2);
-    std::map<std::string, Listing> left(listings.begin(), middle);
-    std::map<std::string, Listing> right(middle, listings.end());
+    //auto middle = std::next(listings.begin(), listings.size() / 2);
+    //std::map<std::string, Listing> left(listings.begin(), middle);
+    //std::map<std::string, Listing> right(middle, listings.end());
 
-    // Recursively sort each half
-    left = mergeSort(sortBy, left);
-    right = mergeSort(sortBy, right);
+    //// Recursively sort each half
+    //left = mergeSort(sortBy, left);
+    //right = mergeSort(sortBy, right);
 
-    // Merge the sorted halves
-    return merge(sortBy, left, right);
+    //// Merge the sorted halves
+    //return merge(sortBy, left, right);
 
-    //if (begin >= end) {
-    //    return;
-    //}
+    if (begin >= end) {
+        return;
+    }
 
-    //q++;
-    //int mid = begin + (end - begin) / 2;
+    q++;
+    int mid = begin + (end - begin) / 2;
 
-    //if (q % 1000 == 0) {
-    //    std::cout << begin << " " << mid << " " << end << std::endl;
-    //}
+    if (q % 1000 == 0) {
+        //std::cout << begin << " " << mid << " " << end << std::endl;
+    }
 
-    //mergeSort(sortBy, listings, begin, mid);
-    //mergeSort(sortBy, listings, mid + 1, end);
-    //merge(sortBy, listings, begin, mid, end);
+    mergeSort(sortBy, listings, begin, mid);
+    mergeSort(sortBy, listings, mid + 1, end);
+    merge(sortBy, listings, begin, mid, end);
 
 }
 
